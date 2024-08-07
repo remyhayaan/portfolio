f551e95133dd49f7920a906ae6dd75eff1f0c64f
Nabukenya Rehemah <remmiehayaan@gmail.com>
2024-08-08 00:05:49 +0300

    changes

--- myportfoli/src/components/Works.js
+++ myportfoli/src/components/Works.js
@@ -1,28 +1,21 @@
-import React from 'react'
-import Portfolio1 from '../assets/portfolio-1.png'
-import Portfolio2 from '../assets/portfolio-2.png'
-import Portfolio3 from '../assets/portfolio-3.png'
-import Portfolio4 from '../assets/portfolio-4.png'
-import Portfolio5 from '../assets/portfolio-5.png'
-import Portfolio6 from '../assets/portfolio-6.png'
-
+import React from 'react';
+import Portfolio1 from '../assets/remy.jpg';
 
 function Works() {
   return (
-   <section id='works'>
-
-    <h2 className='worksTitle'>My Portfolio</h2>
-    <span className='worksDesc'>lorem ipsum......................</span>
-    <div className='worksImgs'>
-        <img src={Portfolio1} alt='p1' className='worksImg'/>
-        <img src={Portfolio2} alt='p2' className='worksImg'/> 
-        <img src={Portfolio3} alt='p3' className='worksImg'/>
-        <img src={Portfolio4} alt='p4' className='worksImg'/>
-        <img src={Portfolio5} alt='p5' className='worksImg'/>
-        <img src={Portfolio6} alt='p6' className='worksImg'/>
-    </div>
-   </section>
-  )
+    <section id='works' className='works-section'>
+      <h2 className='worksTitle'>My Portfolio</h2>
+      <span className='worksDesc'>Below are the projects I have worked on showcasing my skills</span>
+      <div className='worksImgs'>
+        <div className='workItem'>
+          <img src={Portfolio1} alt='p1' className='worksImg'/>
+          <p className='pDesc'>My Portfolio</p>
+        </div>
+       
+       
+      </div>
+    </section>
+  );
 }
 
-export default Works
+export default Works;
